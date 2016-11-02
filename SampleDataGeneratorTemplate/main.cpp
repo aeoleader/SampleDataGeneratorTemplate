1226c55db900aeb610b7a95ebe4c44bcaf92d2e3
aeoleader <aeoleader@gmail.com>
2016-11-02 14:36:12 +0800

    Initial Commit

---
+++ SampleDataGeneratorTemplate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SampleDataGeneratorTemplate
+//
+//  Created by jxkj on 02/11/2016.
+//  Copyright © 2016 jxkj. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

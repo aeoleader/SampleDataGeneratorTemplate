f8c27697dce878819ef9ec1e7196bfe5f4e4d31c
aeoleader <aeoleader@gmail.com>
2016-11-02 15:03:22 +0800

    First Working Version
    —Write to file template

--- SampleDataGeneratorTemplate/main.cpp
+++ SampleDataGeneratorTemplate/main.cpp
@@ -2,14 +2,28 @@
 //  main.cpp
 //  SampleDataGeneratorTemplate
 //
-//  Created by jxkj on 02/11/2016.
+//  Created by Xiangyu on 02/11/2016.
 //  Copyright © 2016 jxkj. All rights reserved.
 //
 
 #include <iostream>
+#include <fstream>
 
+using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    ifstream infile;
+    ofstream outfile("/Users/jxkj/Desktop/PlayGround/CGALTest/test_data.in");
+    //infile.open("", ios::in);
+    //outfile.open("test_data.in", ios::out);
+    
+    if (outfile.is_open())
+    {
+        //Write Data here
+        
+        
+        outfile.close();
+    }
+    
     return 0;
 }
